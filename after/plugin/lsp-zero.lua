8059fd7303eb0ef75f3a8f33c042db4b26abf915
j03m4r <joeemarr12345@gmail.com>
2024-12-09 17:11:06 -0600

    Fixing file buffer attachment issues.

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,4 +1,15 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
+
+vim.opt.signcolumn = "yes"
+
+-- Add cmp_nvim_lsp capabilities settings to lspconfig
+-- This should be executed before you configure any language server
+local lspconfig_defaults = require("lspconfig").util.default_config
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+  "force",
+  lspconfig_defaults.capabilities,
+  require("cmp_nvim_lsp").default_capabilities()
+)
 
 lsp_zero.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
@@ -15,29 +26,65 @@ lsp_zero.on_attach(function(_, bufnr)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = { 'ts_ls','rust_analyzer', 'html' },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = { "rust_analyzer", "ts_ls", "lua_ls", "ember", "cssls", "tailwindcss", "html", "glint" },
     handlers = {
         function(server_name)
-            require('lspconfig')[server_name].setup({
+            require("lspconfig")[server_name].setup({
                 capabilities = lsp_capabilities,
             })
         end,
+        ts_ls = function()
+            require("lspconfig").ts_ls.setup({
+                capabilities = lsp_capabilities,
+                filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+            })
+        end,
+        html = function()
+            require("lspconfig").html.setup({
+                capabilities = lsp_capabilities,
+                filetypes = { "handlebars", "typescriptreact", "html" },
+            })
+        end,
+        tailwindcss = function()
+            require("lspconfig").tailwindcss.setup({
+                capabilities = lsp_capabilities,
+                filetypes = { "handlebars", "typescriptreact", "html" },
+            })
+        end,
+        cssls = function()
+            require("lspconfig").cssls.setup({
+                capabilities = lsp_capabilities,
+                filetypes = { "css" },
+            })
+        end,
+        ember = function()
+            require("lspconfig").ember.setup({
+                capabilities = lsp_capabilities,
+                filetypes = { "handlebars" }, -- Restrict to Handlebars
+            })
+        end,
+        glint = function()
+            require("lspconfig").glint.setup({
+                capabilities = lsp_capabilities,
+                filetypes = { "handlebars" }, -- Restrict to Handlebars
+            })
+        end,
         lua_ls = function()
-            require('lspconfig').lua_ls.setup({
+            require("lspconfig").lua_ls.setup({
                 capabilities = lsp_capabilities,
                 settings = {
                     Lua = {
                         runtime = {
-                            version = 'LuaJIT'
+                            version = "LuaJIT"
                         },
                         diagnostics = {
-                            globals = { 'vim' },
+                            globals = { "vim" },
                         },
                         workspace = {
                             library = {
@@ -51,31 +98,55 @@ require('mason-lspconfig').setup({
     }
 })
 
-local cmp = require('cmp')
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "handlebars",
+--     callback = function()
+--         -- Attach html LSP
+--         require("lspconfig")["html"].setup({
+--             filetypes = {"handlebars"}
+--         })
+--
+--         -- Attach css LSP
+--         require("lspconfig")["tailwindcss"].setup({
+--             filetypes = {"handlebars"}
+--         })
+--
+--         -- Attach ember LSP
+--         require("lspconfig")["ember"].setup({
+--             filetypes = {"handlebars"}
+--         })
+--
+--         require("lspconfig")["glint"].setup({
+--             filetypes = {"handlebars"}
+--         })
+--     end,
+-- })
+
+local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
-require('luasnip.loaders.from_vscode').lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
     sources = {
-        { name = 'path' },
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lua' },
-        { name = 'luasnip', keyword_length = 2 },
-        { name = 'buffer',  keyword_length = 3 },
+        { name = "path" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip", keyword_length = 2 },
+        { name = "buffer",  keyword_length = 3 },
     },
     formatting = lsp_zero.cmp_format({ details = false }),
     mapping = cmp.mapping.preset.insert({
-        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
     }),
-    preselect = 'item',
+    preselect = "item",
     completion = {
-        completeopt = 'menu,menuone,noinsert'
+        completeopt = "menu,menuone,noinsert"
     },
 })
 
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,9 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "java", "typescript", "python", "c", "lua", "vim", "vimdoc", "query", "markdown_inline" },
+  ensure_installed = { "javascript", "java", "typescript", "python", "c", "lua", "vim", "vimdoc", "query", "markdown_inline", "html", "htmldjango", "glimmer" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
